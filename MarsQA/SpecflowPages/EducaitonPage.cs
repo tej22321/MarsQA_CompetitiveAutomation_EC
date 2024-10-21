5635ef0450fed6056f0d0f1613e3ee7425a6e62d
tej22321 <tejaswi.pinisetty@gmail.com>
2024-10-21 11:25:45 +1100

    intial commit , included the skeleton code of automation for certification and education functionalituies, included the extent report and json reader packages

---
+++ MarsQA/SpecflowPages/EducaitonPage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsQA.SpecflowPages
+{
+    internal class EducaitonPage
+    {
+    }
+}
